bdd16db16774474a926d8326074ee1c1dfc106fd
zenicolas <zenicolas@gmail.com>
2024-08-27 12:39:45 +0200

    Update cargo_shipment.py

--- cargo_management/shipment_management/doctype/cargo_shipment/cargo_shipment.py
+++ cargo_management/shipment_management/doctype/cargo_shipment/cargo_shipment.py
@@ -41,11 +41,11 @@ class CargoShipment(Document):
             'packages': get_list_from_child_table(self.cargo_shipment_lines, 'package')
         })
 
-        wrs_in_cs = get_list_from_child_table(self.cargo_shipment_lines, 'warehouse_receipt')
-        if wrs_in_cs:  # If empty we don't touch the DB  # FIXME: Performance?
-            frappe.db.sql("UPDATE `tabWarehouse Receipt` SET departure_date = %(date)s WHERE name IN %(wrs_in_cs)s", {
-                'date': self.departure_date, 'wrs_in_cs': wrs_in_cs
-            })
+        # wrs_in_cs = get_list_from_child_table(self.cargo_shipment_lines, 'warehouse_receipt')
+        # if wrs_in_cs:  # If empty we don't touch the DB  # FIXME: Performance?
+        #    frappe.db.sql("UPDATE `tabWarehouse Receipt` SET departure_date = %(date)s WHERE name IN %(wrs_in_cs)s", {
+        #        'date': self.departure_date, 'wrs_in_cs': wrs_in_cs
+        #    })
 
     def change_status(self, new_status):
         """ Validates the current status of the cargo shipment and change it if it's possible. """
