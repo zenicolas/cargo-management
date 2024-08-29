910b4f9a85fc90477df2bee8f2e887d92dae495f
zenicolas <zenicolas@gmail.com>
2024-08-29 11:23:25 +0300

    Update actions.py

--- cargo_management/shipment_management/doctype/cargo_shipment/actions.py
+++ cargo_management/shipment_management/doctype/cargo_shipment/actions.py
@@ -2,6 +2,7 @@ import frappe
 from cargo_management.utils import get_list_from_child_table, update_status_in_bulk
 
 
+<<<<<<< HEAD
  @frappe.whitelist(methods='POST')
  def update_status(source_doc_name: str, new_status: str, msg_title: str):
      doc = frappe.get_cached_doc('Cargo Shipment', source_doc_name)  # Getting the Cargo Shipment Doc from db
@@ -18,3 +19,17 @@ from cargo_management.utils import get_list_from_child_table, update_status_in_b
 	 # TODO: Filter Unique
 
 #         'Warehouse Receipt': wrs_in_cs,
+=======
+@frappe.whitelist(methods='POST')
+def update_status(source_doc_name: str, new_status: str, msg_title: str):
+    doc = frappe.get_cached_doc('Cargo Shipment', source_doc_name)  # Getting the Cargo Shipment Doc from db
+
+    wrs_in_cs = get_list_from_child_table(doc.cargo_shipment_lines, 'warehouse_receipt')  # TODO: Filter Unique
+    packages_in_cs = get_list_from_child_table(doc.cargo_shipment_lines, 'package')
+
+    update_status_in_bulk(docs_to_update={
+        'Cargo Shipment': [doc.name],
+        'Warehouse Receipt': wrs_in_cs,
+        'Parcel': packages_in_cs
+    }, new_status=new_status, msg_title=msg_title, mute_emails=doc.mute_emails)  # todo: Work on msg_title
+>>>>>>> parent of e58c180 (Update actions.py)
